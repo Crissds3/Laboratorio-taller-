f159e3d2d2d42a0224e03f2f46830f7f804a907d
Sebastián <321seba1retamal2r@gmail.com>
2025-04-23 10:25:45 -0400

    Backend Impresion

--- backend/Routes/universalRoutes.js
+++ backend/Routes/universalRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const loginRoutes = require('./LoginRoutes.js');
 const registerRoutes = require('./RegisterRoutes.js');
+const impresionRoutes = require('./impresionRoutes');
 
 router.use('/auth', loginRoutes);
 router.use('/auth', registerRoutes);
-
+router.use('/api', impresionRoutes);
 module.exports = router;
 
 // ESTE ARCHIVO SE ENCARGA DE LAS RUTAS GENERALES DE LA APLICASAO
